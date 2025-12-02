44e0669983393ad3c7ac7bad5a6dd31ce2733bee
Mykhailo Litvinov <mlitvino@student.hive.fi>
2025-12-02 15:27:50 +0200

    part2/notes: extract notes service to separate file

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,24 +1,20 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
-  const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
-  }
-
-  useEffect(hook, [])
-  console.log('render', notes.length, 'notes')
+  }, [])
 
   const addNote = (event) => {
     event.preventDefault()
@@ -28,10 +24,10 @@ const App = () => {
       id: notes.length + 1
     }
 
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data))
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
   }
@@ -45,13 +41,14 @@ const App = () => {
     : notes.filter((note) => note.important)
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
-  
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(note => note.id === id ? response.data : note))
-    })
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id === id ? returnedNote : note))
+      })
   }
 
   return (
