abaa65ad91e3f8ec09f67b2403eb6a41eb3e6b4a
Misha <darkrabbit240@gmail.com>
2026-08-05 16:15:45 +0300

    part8-3: add phonebook

---
+++ part8/3/frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import { useQuery } from '@apollo/client/react'
+import { useState } from 'react'
+
+import PhoneForm from './components/PhoneForm'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+import Notify from './components/Notify'
+import { ALL_PERSONS } from './queries'
+
+const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  return (
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons = {result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
+    </div>
+  )
+
+}
+
+export default App
