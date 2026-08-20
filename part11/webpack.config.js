653b49ef054571bdf04d7a63bcbfade27a869d7e
Misha <darkrabbit240@gmail.com>
2026-08-20 16:26:13 +0300

    fix build and test

--- part11/webpack.config.js
+++ part11/webpack.config.js
@@ -2,6 +2,9 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
 module.exports = {
+  performance: {
+    hints: false,
+  },
   entry: "./src/index.jsx",
   output: {
     path: path.resolve(__dirname, 'dist'),
